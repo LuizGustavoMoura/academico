cb65fa03de683ba117709c6e381a5b902ad4ee05
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-02-15 21:11:49 -0300

    primeiro commit deste projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Acadêmico</title>
+</head>
+<body>
+    <img src="imagens/logosenacv2.png" alt="logo senac" width="200px" height="auto">
+    <h1>Controle Acadêmico</h1>
+    <p>Bem-vindo ao <b>sistema de controle acadêmico</b>.</p> <p>Aqui, você poderá consultar suas notas.</p> <p>E também sua frequência.</p>
+</body>
+</html>
