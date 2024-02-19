bcc2de3fe9de2ffe770763cd2fe1bb05131c049c
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-02-19 18:42:11 -0300

    criação do menu

--- index.php
+++ index.php
@@ -9,5 +9,15 @@
     <img src="imagens/logosenacv2.png" alt="logo senac" width="200px" height="auto">
     <h1>Controle Acadêmico</h1>
     <p>Bem-vindo ao <b>sistema de controle acadêmico</b>.</p> <p>Aqui, você poderá consultar suas notas.</p> <p>E também sua frequência.</p>
+    <h2>Menu</h2>
+    <ul>
+        <li><a href="aluno.php">Alunos</a></li>
+        <li><a href="professor.php">Professores</a></li>
+        <li><a href="disciplina.php">Disciplinas</a></li>
+        <li><a href="turma.php">Turmas</a></li>
+        <li><a href="nota.php">Notas</a></li>
+        <li><a href="frequencia.php">Frequência</a></li>
+    </ul>
+    
 </body>
 </html>
