8c8a10a891d87a6f86dac3097943789d0ff102f0
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-02-19 20:03:33 -0300

    criação da lista de professores

---
+++ professor.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle Acadêmico</title>
+    <link rel="stylesheet" href="css/principal.css">
+</head>
+<body>
+    <h1>Professores</h1>
+    <table>
+        <tr>
+            <th>Disciplina</th>
+            <th>Professor(a)</th>
+        </tr>
+        <tr>
+            <td>Matemática</td>
+            <td>Professor Pedro do Vale</td>
+        </tr>
+        <tr>
+            <td>Português</td>
+            <td>Professora Bianca Lacerda</td>
+        </tr>
+    </table>
+</body>
+</html>
