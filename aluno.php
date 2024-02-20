c9d6023253fb9f66e07d6ac237a5170af5d1ae88
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-02-20 20:49:35 -0300

    cálculo da média das notas

--- aluno.php
+++ aluno.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Controle Acadêmico</title>
     <link rel="stylesheet" href="css/principal.css">
+    <script type="text/javascript" src="script/codigo.js"></script>
 </head>
 <body id="aluno">
     <a href="index.php">Início</a>
@@ -12,17 +13,33 @@
     <h1>Cadastro dos Alunos</h1>
     
     <form action="" method="post">
-        <label for="matricula">Matrícula: </label>
-        <input type="text" name="matricula" id="matricula" placeholder="Digite a matrícula">
-        <label for="nome">Nome: </label>
-        <input type="text" name="nome" id="nome" placeholder="Digite o nome do aluno">
-        <label for="datanascimento">Data de Nascimento: </label>
-        <input type="date" name="datanascimento" id="datanascimento">
-        <label for="genero">Gênero: </label>
-        <input type="radio" name="genero" id="genero" value="Masculino">Masculino
-        <input type="radio" name="genero" id="genero" value="Feminino">Feminino
+        <p>
+            <label for="matricula">Matrícula: </label>
+            <input type="text" name="matricula" id="matricula" placeholder="Digite a matrícula">
+        </p>
+        <p>
+            <label for="nome">Nome: </label>
+            <input type="text" name="nome" id="nome" placeholder="Digite o nome do aluno">
+        </p>
+        <p>
+            <label for="datanascimento">Data de Nascimento: </label>
+            <input type="date" name="datanascimento" id="datanascimento">
+        </p>
+        <p>
+            <label for="genero">Gênero: </label>
+            <input type="radio" name="genero" id="genero" value="Masculino">Masculino
+            <input type="radio" name="genero" id="genero" value="Feminino">Feminino
+        </p>
+        <p>
+            <label for="nota1">Primeira Nota</label>
+            <input type="number" name="nota1" id="nota1" min="0" max="10">
+        </p>
+        <p>
+            <label for="nota2">Segunda Nota</label>
+            <input type="number" name="nota2" id="nota2" min="0" max="10">
+        </p>
 
-        <button type="submit" class="btn btn-primary btn-lg" value="Enviar">Enviar</button>
+        <button type="button" value="Calcular a média" onclick="resultado(this.form)">Calcular a média</button>
     </form>
 </body>
 </html>
---
+++ script/codigo.js
@@ -0,0 +1,20 @@
+document.write("Data Atual: ");
+document.write(Date());
+
+function funcao1() {
+    alert("Usando Função");
+}
+
+function calcmedia(n1, n2) {
+    var m = (n1+n2)/2;
+    return m;
+}
+
+function resultado(form) {
+    var media = calcmedia(parseFloat(form.nota1.value), parseFloat(form.nota2.value));
+    if (media>=7) {
+        alert("Aprovado com a média " + media);
+    } else {
+        alert("Reprovado com a média " + media);
+    }
+}
