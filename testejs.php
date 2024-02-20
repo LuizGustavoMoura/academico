03f17d1365f1fa8352ffa2faf79267c31d902aa1
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-02-20 20:50:14 -0300

    testando JavaScript

---
+++ testejs.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle Acadêmico</title>
+</head>
+<body>
+    <h1>Sistema Acadêmico</h1>
+    <p>Menu Principal</p>
+
+    <button type="button" onclick="funcao1()">Clique Aqui</button>
+
+    <script type="text/javascript" src="script/codigo.js">
+        
+    </script>
+</body>
+</html>
