9fd153d514de9e8e4c6fd4f52b02da023aa572f4
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-03-11 20:46:40 -0300

    atualizar curso

---
+++ atualizar_curso.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['id'], $_POST['nome'], $_POST['curso'])) {
+        $id = $_POST['id'];
+        $nome = $_POST['nome'];
+        $curso = $_POST['curso'];
+    
+
+        //Conexão com o bando de dados
+        require('script/conexao.php');
+
+        //Atualize o curso no banco de dados
+        $sql = "UPDATE matricula SET nome='$nome', curso='$curso' WHERE id=$id";
+        
+        $resultado = mysqli_query($conexao, $sql);
+        if ($resultado) {
+            echo "Curso atualizado com sucesso!";
+            header("Location:cadastrocurso.php");
+        } else {
+            echo "Erro ao atualizar o curso" . mysqli_error($conexao);
+        }
+
+        mysqli_close($conexao);
+    } else {
+        echo "Preencha os dados";
+    }
+    
+} else {
+    header("Location:cadastrocurso.php");
+    exit;
+}
+?>
