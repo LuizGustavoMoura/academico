f98a60831c7190143e4dd94685185d1df512c8ee
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-03-12 20:52:46 -0300

    criação do login

--- index.php
+++ index.php
@@ -3,38 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema Acadêmico</title>
-    <link rel="stylesheet" href="css/principal.css">
+    <title>Document</title>
 </head>
-<body id="inicio">
-    <img src="imagens/logosenacv2.png" alt="logo senac" width="200px" height="auto">
-    <h1>Controle Acadêmico</h1>
-    <p>Bem-vindo ao <b>sistema de controle acadêmico</b>.</p> <p>Aqui, você poderá consultar suas <a href="notas.php" alt="notas da turma" target="_self">notas</a>.</p> <p>E também sua <i><a href="frequencia.php" alt="frequencia dos alunos">frequência</a></i>.</p>
-    <h2>Menu</h2>
-    <ul>
-        <li><a href="aluno.php">Alunos</a></li>
-        <li><a href="professor.php">Professores</a></li>
-        <li><a href="disciplina.php">Disciplinas</a></li>
-        <li><a href="turma.php">Turmas</a></li>
-        <li><a href="nota.php">Notas</a></li>
-        <li><a href="frequencia.php">Frequência</a></li>
-    </ul>
+<body>
+    <h2>Login</h2>
 
-    <?php
-    // Meu primeiro código
+    <form action="login.php" method="post">
+        <p>
+            <label for="usuario">Usuário: </label>
+            <input type="text" name="usuario" id="usuario">
+        </p>
+        <p>
+            <label for="senha">Senha: </label>
+            <input type="password" name="senha" id="senha">
+        </p>
 
-    echo '<h2>'.'Sistema Acadêmico'.'</h2>';
-
-    $aluno = '<p>'.'Luiz Gustavo'.'</p>';
-    $curso = "Análise e Desenvolvimento de Sistemas";
-
-    /* Esse é um comentário */
-
-    echo "O aluno: ".$aluno."Frequenta o curso: ".$curso;
-
-    $notas = array("prova1" => 8.3, "prova2" => 5.9, "prova3" => 9.2);
-
-    echo '<p>'.($notas["prova1"]+$notas["prova2"]+$notas["prova3"])/3,'</p>';
-    ?>
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
