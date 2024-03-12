b9947f9b0eb019991410ba6eebf2abd51d7600c9
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-03-12 20:53:37 -0300

    login

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
@@ -37,4 +37,36 @@
     </form>
 </body>
 
-</html>
+</html> -->
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['usuario'], $_POST['senha'])) {
+            $usuario = $_POST['usuario'];
+            $senha = $_POST['senha'];
+
+            require ('script/conexao.php');
+
+            $sql = "SELECT * FROM usuarios WHERE usuario='$usuario' AND senha='$senha'";
+
+            $resultado = mysqli_query($conexao, $sql);
+
+            if (mysqli_num_rows($resultado) == 1) {
+                session_start();
+                $_SESSION['usuario'] = $usuario;
+
+                header("Location:dashboard.php");
+                exit;
+            } else {
+                echo "Usuário ou senha inválido(a)";
+            }
+
+            mysqli_close($conexao);
+        } else {
+            echo "Por favor, preencha os campos";
+        }
+    } else {
+        header("Location:dashboard.php");
+        exit;
+    }
+?>
