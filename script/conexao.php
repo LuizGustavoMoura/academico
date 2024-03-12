e22d61ccc317437c57dc698e72f688b17414b8d3
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-03-12 20:54:24 -0300

    conexão

--- script/conexao.php
+++ script/conexao.php
@@ -1,18 +1,18 @@
 <?php
-// $hostname = 'localhost';
-// $username = 'root';
-// $password = '';
-// $database = 'cafeteria';
-// $port = '3307';
+    // $hostname = 'localhost';
+    // $username = 'root';
+    // $password = '';
+    // $database = 'cafeteria';
+    // $port = '3307';
 
-// Tente estabelecer a conexão
-$conexao = mysqli_connect("localhost", "root", "", "academico", "3307");
+    // Tente estabelecer a conexão
+    $conexao = mysqli_connect("localhost", "root", "", "academico", "3307");
 
-// Verifique se a conexão foi bem-sucedida
-if (!$conexao) {
-    die('Não foi possível conectar ao banco de dados' . mysqli_connect_error());
-}
+    // Verifique se a conexão foi bem-sucedida
+    if (!$conexao) {
+        die('Não foi possível conectar ao banco de dados' . mysqli_connect_error());
+    }
 
-echo 'Conexão bem sucedida!';
+    echo 'Conexão bem sucedida!';
 ?>
 
