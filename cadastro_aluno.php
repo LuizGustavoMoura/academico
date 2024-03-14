b512a3d671827b14be0cf884e1fc87fd8be523f1
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-03-14 20:56:45 -0300

    cadastro aluno

---
+++ cadastro_aluno.php
@@ -0,0 +1,25 @@
+<?php
+    $nome = $_POST['aluno'];
+    $cpf = $_POST['cpf'];
+    $endereco = $_POST['endereco'];
+    $complemento = $_POST['complemento'];
+    $cep = $_POST['cep'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+    $telefone = $_POST['telefone'];
+
+    require('script/conexao.php');
+
+    $sql = "INSERT INTO aluno (nome, cpf, endereco, complemento, cep, bairro, cidade, estado, telefone) VALUES ('$nome', '$cpf', '$endereco', '$complemento', '$cep', '$bairro', '$cidade', '$estado', '$telefone')";
+
+    if (mysqli_query($conexao, $sql)) {
+        echo "Registro inserido com sucesso";
+    } else {
+        echo "Erro ao inserir o registro: " . mysqli_error($conexao);
+    }
+
+    mysqli_close($conexao);
+
+    header('Location: cadastroaluno.php');
+?>
