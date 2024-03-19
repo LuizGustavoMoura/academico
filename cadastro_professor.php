2914066dbb9c1b86ed8150ca34bf09a9cd71c6fc
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-03-18 21:04:44 -0300

    cadastro professor

---
+++ cadastro_professor.php
@@ -0,0 +1,27 @@
+<?php
+    $nome = $_POST['professor'];
+    $cpf = $_POST['cpf'];
+    $endereco = $_POST['endereco'];
+    $complemento = $_POST['complemento'];
+    $cep = $_POST['cep'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
+    $estado = $_POST['estado'];
+    $telefone = $_POST['telefone'];
+    $formacao = $_POST['formacao'];
+    $titulacao = $_POST['titulacao'];
+
+    require('script/conexao.php');
+
+    $sql = "INSERT INTO aluno (nome, cpf, endereco, complemento, cep, bairro, cidade, estado, telefone, formacao, titulacao) VALUES ('$nome', '$cpf', '$endereco', '$complemento', '$cep', '$bairro', '$cidade', '$estado', '$telefone', '$formacao', '$titulacao')";
+
+    if (mysqli_query($conexao, $sql)) {
+        echo "Registro inserido com sucesso";
+    } else {
+        echo "Erro ao inserir o registro: " . mysqli_error($conexao);
+    }
+
+    mysqli_close($conexao);
+
+    header('Location: cadastroprofessor.php');
+?>
