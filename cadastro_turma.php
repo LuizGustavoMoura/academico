3e4e38acc4b16bb82eda8fd152525941b6c2dfbf
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-04 20:16:50 -0300

    cadastro turma

---
+++ cadastro_turma.php
@@ -0,0 +1,18 @@
+<?php
+    $iddisciplina = $_POST['disciplina'];
+    $idprofessor = $_POST['professor'];
+
+    require('script/conexao.php');
+
+    $sql = "INSERT INTO turma (iddisciplina, idprofessor) VALUES ('$iddisciplina', '$idprofessor')";
+
+    if (mysqli_query($conexao, $sql)) {
+        echo "Registro inserido com sucesso";
+    } else {
+        echo "Erro ao inserir o registro: " . mysqli_error($conexao);
+    }
+
+    mysqli_close($conexao);
+
+    // header('Location: cadastroturma.php');
+?>
