614b0e468feefd3907f8c2e0a8ea05a611fc6955
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-05 19:05:30 -0300

    cadastro de curso

--- cadastro_curso.php
+++ cadastro_curso.php
@@ -1,22 +1,19 @@
 <?php
+    $nome = $_POST['nome'];
+    $duracao = $_POST['duracao'];
+    $coordenador = $_POST['coordenador'];
+    $nivel = $_POST['nivel'];
+    $modalidade = $_POST['modalidade'];
 
-$nome = $_POST['nome'];
-$duracao = $_POST['duracao'];
-$coordenador = $_POST['coordenador'];
-$nivel = $_POST['nivel'];
-$modalidade = $_POST['modalidade'];
+    require ('script/conexao.php'); 
 
-require ('script/conexao.php'); 
+    $sql = "INSERT INTO curso (nome, duracao, coordenador, nivel, modalidade) VALUES ('$nome','$duracao', '$coordenador', '$nivel', '$modalidade')";
 
-$sql = "INSERT INTO curso (nome, duracao, coordenador, nivel, modalidade) VALUES ('$nome','$duracao', '$coordenador', '$nivel', '$modalidade')";
+    if (mysqli_query($conexao, $sql)) {
+        echo "Registro inserido com sucesso!";
+    } else {
+        echo "Erro ao inserir registro: " . mysqli_error($conexao);
+    }
 
-if (mysqli_query($conexao, $sql)) {
-    echo "Registro inserido com sucesso!";
-} else {
-    echo "Erro ao inserir registro: " . mysqli_error($conexao);
-}
-
-header("location:cadastrodecurso.php");
-
-
+    header("location:cadastrodecurso.php");
 ?>
--- cadastrodecurso.php
+++ cadastrodecurso.php
@@ -1,68 +1,66 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sistema Acadêmico</title>
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema Acadêmico</title>
-</head>
+    <body>
+        <p>Preencher os dados do curso(a):</p>
 
-<body>
-    <p>Preencher os dados do curso(a):</p>
+        <fieldset>
+            <legend>Cadastro de Curso(a)</legend>
 
-    <fieldset>
-        <legend>Cadastro de Curso(a)</legend>
+            <form action="cadastro_curso.php" method="post">
+                <p>
+                    <label for="nome">Nome: </label>
+                    <input type="text" name="nome" id="nome">
+                </p>
+                <p>
+                    <label for="duracao">Duração: </label>
+                    <input type="text" name="duracao" id="duracao">
+                </p>
+                <p>
+                    <label for="coordenador">Coordenador(a): </label>
+                    <select name="coordenador" id="coordenador">
+                        <?php
+                            require ('script/conexao.php');
 
-        <form action="cadastro_curso.php" method="post">
-            <p>
-                <label for="nome">Nome: </label>
-                <input type="text" name="nome" id="nome">
-            </p>
-            <p>
-                <label for="duracao">Duração: </label>
-                <input type="text" name="duracao" id="duracao">
-            </p>
-            <p>
-                <label for="coordenador">Coordenador(a): </label>
-                <select name="coordenador" id="coordenador">
-                    <?php
-                        require ('script/conexao.php');
+                            $sql = "SELECT cpf, nome FROM professor";
+                            $resultado = mysqli_query($conexao, $sql);
+                            
+                            while ($row = mysqli_fetch_assoc($resultado)) {
+                                echo "<option value='{$row['cpf']}'>{$row['nome']}</option>";
+                            }
+                        ?>
+                    </select>
+                </p>
+                <p>
+                    <label for="nivel">Nível:</label>
+                    <select name="nivel" id="nivel">
+                        <option value="">Selecione o nível do curso</option>
+                        <option value="tecnico">Técnico</option>
+                        <option value="tecnologo">Tecnólogo</option>
+                        <option value="bacharelado">Bacharelado</option>
+                        <option value="licenciatura">Licenciatura</option>
+                        <option value="especializacao">Especialização</option>
+                    </select>
+                </p>
+                <p>
+                    <label for="modalidade">Modalidade:</label>
+                    <select name="modalidade" id="modalidade">
+                        <option value="">Selecione a modalidade do curso</option>
+                        <option value="presencial">Presencial</option>
+                        <option value="ead">Educação à distância (EAD)</option>
+                    </select>
+                </p>
+                </fieldset>
 
-                        $sql = "SELECT cpf, nome FROM professor";
-                        $resultado = mysqli_query($conexao, $sql);
-                        
-                        while ($row = mysqli_fetch_assoc($resultado)) {
-                            echo "<option value='{$row['cpf']}'>{$row['nome']}</option>";
-                        }
-                    ?>
-                </select>
-            </p>
-            <p>
-                <label for="nivel">Nível:</label>
-                <select name="nivel" id="nivel">
-                    <option value="">Selecione o nível do curso</option>
-                    <option value="tecnico">Técnico</option>
-                    <option value="tecnologo">Tecnólogo</option>
-                    <option value="bacharelado">Bacharelado</option>
-                    <option value="licenciatura">Licenciatura</option>
-                    <option value="especializacao">Especialização</option>
-                </select>
-            </p>
-            <p>
-                <label for="modalidade">Modalidade:</label>
-                <select name="modalidade" id="modalidade">
-                    <option value="">Selecione a modalidade do curso</option>
-                    <option value="presencial">Presencial</option>
-                    <option value="ead">Educação à distância (EAD)</option>
-                </select>
-            </p>
-            </fieldset>
-
-            <p>
-                <input type="submit" value="Enviar">
-                <input type="reset" value="Limpar">
-            </p>
-        </form>
-</body>
-
+                <p>
+                    <input type="submit" value="Enviar">
+                    <input type="reset" value="Limpar">
+                </p>
+            </form>
+    </body>
 </html>
