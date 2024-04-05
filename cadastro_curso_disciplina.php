daf7cdb1c75612443eb8f332a3e0c5a8a71d8c5a
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-04 21:01:45 -0300

    cadastro_curso_disciplina

---
+++ cadastro_curso_disciplina.php
@@ -0,0 +1,16 @@
+<?php
+    $idcurso = $_POST['curso'];
+    $iddisciplina = $_POST['disciplina'];
+
+    require ('script/conexao.php');
+
+    $sql = "INSERT INTO curso_disciplina (idcurso, iddisciplina) VALUES ('$idcurso', '$iddisciplina')";
+
+    if (mysqli_query($conexao, $sql)) {
+        echo "Registro inserido com sucesso!";
+    } else {
+        echo "Erro ao inserir registro: ".mysqli_error($conexao);
+    }
+
+    header('Location: cadastrocursodisciplina.php');
+?>
---
+++ cadastrocursodisciplina.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sistema Acadêmico</title>
+    </head>
+
+    <body>
+        <p>Preencher os dados da turma:</p>
+
+        <fieldset>
+            <legend>Cadastro de Disciplina nos Cursos</legend>
+
+            <form action="cadastro_curso_disciplina.php" method="post">
+                <p>
+                    <label for="curso">Curso: </label>
+                    <select name="curso" id="curso">
+                        <?php
+                            require ('script/conexao.php');
+                            $sql = "SELECT idcurso, nome FROM curso";
+                            $resultado = mysqli_query($conexao, $sql);
+                            while ($row = mysqli_fetch_assoc($resultado)) {
+                                echo "<option value='{$row['id']}'>{$row['nome']}</option>";
+                            }
+                        ?>
+                    </select>
+                </p>
+                <p>
+                    <label for="disciplina">Disciplina: </label>
+                    <select name="disciplina" id="disciplina">
+                        <?php
+                            require ('script/conexao.php');
+                            $sql = "SELECT iddisciplina, nome FROM disciplina";
+                            $resultado = mysqli_query($conexao, $sql);
+                            while ($row = mysqli_fetch_assoc($resultado)) {
+                                echo "<option value='{$row['id']}'>{$row['nome']}</option>";
+                            }
+                        ?>
+                    </select>
+                </p>
+                <p>
+                    <input type="submit" value="Enviar">
+                    <input type="reset" value="Limpar">
+                </p>
+            </form>
+    </body>
+</html>
