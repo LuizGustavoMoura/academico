3d502a03a05678e1545b4172bb4fa1e0fed605f7
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-05 19:12:20 -0300

    testando JavaScript

--- testejs.php
+++ testejs.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controle Acadêmico</title>
-</head>
-<body>
-    <h1>Sistema Acadêmico</h1>
-    <p>Menu Principal</p>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Controle Acadêmico</title>
+    </head>
 
-    <button type="button" onclick="funcao1()">Clique Aqui</button>
-
-    <script type="text/javascript" src="script/codigo.js">
+    <body>
+        <h1>Sistema Acadêmico</h1>
         
-    </script>
-</body>
+        <p>Menu Principal</p>
+
+        <button type="button" onclick="funcao1()">Clique Aqui</button>
+
+        <script type="text/javascript" src="script/codigo.js">
+            
+        </script>
+    </body>
 </html>
