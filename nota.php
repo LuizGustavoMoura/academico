1353c873deb7159196cff1a86837a998dde06775
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-05 19:10:40 -0300

    nota

--- nota.php
+++ nota.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cálculo das Médias</title>
-</head>
-<body>
-    <form action="media.php" method="post">
-        <a href="index.php">Início</a>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cálculo das Médias</title>
+    </head>
 
-        <h1>Nota do Aluno</h1>
+    <body>
+        <form action="media.php" method="post">
+            <a href="index.php">Início</a>
 
-        <p>
-            <label for="nome">Nome do aluno: </label>
-            <input type="text" name="nome" id="nome">
-        </p>
-        <p>
-            <label for="p1">Nota da primeira prova: </label>
-            <input type="text" name="p1" id="p1">
-        </p>
-        <p>
-            <label for="p2">Nota da segunda prova: </label>
-            <input type="text" name="p2" id="p2">
-        </p>
-        <p>
-            <label for="falta">Quantidade de faltas: </label>
-            <input type="text" name="falta" id="falta">
-        </p>
+            <h1>Nota do Aluno</h1>
 
-        <input type="submit" value="Enviar">
-    </form>
-</body>
+            <p>
+                <label for="nome">Nome do aluno: </label>
+                <input type="text" name="nome" id="nome">
+            </p>
+            <p>
+                <label for="p1">Nota da primeira prova: </label>
+                <input type="text" name="p1" id="p1">
+            </p>
+            <p>
+                <label for="p2">Nota da segunda prova: </label>
+                <input type="text" name="p2" id="p2">
+            </p>
+            <p>
+                <label for="falta">Quantidade de faltas: </label>
+                <input type="text" name="falta" id="falta">
+            </p>
+
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
 </html>
