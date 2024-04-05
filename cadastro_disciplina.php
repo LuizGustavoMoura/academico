27ef8fb3202f2bb1b6429f23eb1a0ce2510e9558
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-05 19:05:52 -0300

    cadastro disciplina

--- cadastro_disciplina.php
+++ cadastro_disciplina.php
@@ -1,21 +1,18 @@
 <?php
+    $nome = $_POST['nome'];
+    $carga = $_POST['carga'];
+    $creditos = $_POST['creditos'];
+    $emenda = $_POST['ementa'];
 
-$nome = $_POST['nome'];
-$carga = $_POST['carga'];
-$creditos = $_POST['creditos'];
-$emenda = $_POST['ementa'];
+    require ('script/conexao.php'); 
 
-require ('script/conexao.php'); 
+    $sql = "INSERT INTO curso (nome, carga, creditos, ementa) VALUES ('$nome','$carga', '$creditos', '$ementa')";
 
-$sql = "INSERT INTO curso (nome, carga, creditos, ementa) VALUES ('$nome','$carga', '$creditos', '$ementa')";
+    if (mysqli_query($conexao, $sql)) {
+        echo "Registro inserido com sucesso!";
+    } else {
+        echo "Erro ao inserir registro: " . mysqli_error($conexao);
+    }
 
-if (mysqli_query($conexao, $sql)) {
-    echo "Registro inserido com sucesso!";
-} else {
-    echo "Erro ao inserir registro: " . mysqli_error($conexao);
-}
-
-header("location:cadastrodisciplina.php");
-
-
+    header("location:cadastrodisciplina.php");
 ?>
