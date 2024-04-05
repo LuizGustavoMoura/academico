b9d8e0ccb33c7eefdc55a0b8aa3a9c8cbd8224b2
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-05 19:07:29 -0300

    cadastro matrícula

--- cadastromatricula.php
+++ cadastromatricula.php
@@ -1,48 +1,50 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema Academico</title>
-</head>
-<body>
-    <p>Selecione o Aluno e o Curso</p>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sistema Academico</title>
+    </head>
 
-    <fieldset>
-        <legend>Matrícula</legend>
-    <form action="cadastro_matricula.php" method="post">
-    <p>
-        <label for="aluno">Selecione o aluno:</label>
-        <select name="aluno" id="aluno">
-            <?php
-                require ('script/conexao.php');
-                $sql = "SELECT cpf,nome FROM aluno";
-                $resultado = mysqli_query($conexao, $sql);
-                while ($row = mysqli_fetch_assoc($resultado)) {
-                    echo "<option value='{$row['cpf']}'>{$row['nome']}</option>";
-                }
-            ?>
-        </select>
-    </p>
-    <p>
-        <label for="curso">Selecione o curso:</label>
-        <select name="curso" id="curso">
-            <?php
-                require ('script/conexao.php');
-                $sql = "SELECT id,nome FROM curso";
-                $resultado = mysqli_query($conexao, $sql);
-                while ($row = mysqli_fetch_assoc($resultado)) {
-                    echo "<option value='{$row['id']}'>{$row['nome']}</option>";
-                }
-            ?>
-        </select>
-    </p>
-    </fieldset>
-   <p>   
-    <input type="reset" value="Limpar">
-    <input type="submit" value="Enviar">
-    </p>   
-</form>
-    
-</body>
+    <body>
+        <p>Selecione o Aluno e o Curso</p>
+
+        <fieldset>
+            <legend>Matrícula</legend>
+            
+            <form action="cadastro_matricula.php" method="post">
+                <p>
+                    <label for="aluno">Selecione o aluno:</label>
+                    <select name="aluno" id="aluno">
+                        <?php
+                            require ('script/conexao.php');
+                            $sql = "SELECT cpf,nome FROM aluno";
+                            $resultado = mysqli_query($conexao, $sql);
+                            while ($row = mysqli_fetch_assoc($resultado)) {
+                                echo "<option value='{$row['cpf']}'>{$row['nome']}</option>";
+                            }
+                        ?>
+                    </select>
+                </p>
+                <p>
+                    <label for="curso">Selecione o curso:</label>
+                    <select name="curso" id="curso">
+                        <?php
+                            require ('script/conexao.php');
+                            $sql = "SELECT id,nome FROM curso";
+                            $resultado = mysqli_query($conexao, $sql);
+                            while ($row = mysqli_fetch_assoc($resultado)) {
+                                echo "<option value='{$row['id']}'>{$row['nome']}</option>";
+                            }
+                        ?>
+                    </select>
+                </p>
+                </fieldset>
+
+                <p>   
+                    <input type="reset" value="Limpar">
+                    <input type="submit" value="Enviar">
+                </p>   
+            </form>
+    </body>
 </html>
