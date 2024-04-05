96078978e92330b06ce976332efe38d8d3eaafa2
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-05 19:10:57 -0300

    salvar curso

--- salvarcurso.php
+++ salvarcurso.php
@@ -1,78 +1,79 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Curso</title>
-</head>
-<body>
-    <?php
-        $nome = $_POST['aluno'];
-        $curso = $_POST['curso'];
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cadastro de Curso</title>
+    </head>
 
-        echo '<h2>'.'Confira os dados'.'</h2>';
+    <body>
+        <?php
+            $nome = $_POST['aluno'];
+            $curso = $_POST['curso'];
 
-        echo '<p>'.$nome.'</p>';
+            echo '<h2>'.'Confira os dados'.'</h2>';
 
-        switch ($curso) {
-            case 'm':
-                echo '<p>'.'Manutenção de Computadores'.'</p>';
-                break;
-            case 'r':
-                echo '<p>'.'Redes de Computadores'.'</p>';
-                break;
-            case 'p':
-                echo '<p>'.'Programação de Computadores'.'</p>';
-                break;
-            case 'w':
-                echo '<p>'.'Programação Web'.'</p>';
-                break;
-            
-            default:
-                echo '<p>'.'Digite um código válido (m, r, p ou w)'.'</p>';
-                break;
-        }
+            echo '<p>'.$nome.'</p>';
 
-        //vetor
-        //matriz
-        //$grupo = array("Álvaro", "Kelly", "Letícia", "Rodrigo");
-        //$i = 0;
+            switch ($curso) {
+                case 'm':
+                    echo '<p>'.'Manutenção de Computadores'.'</p>';
+                    break;
+                case 'r':
+                    echo '<p>'.'Redes de Computadores'.'</p>';
+                    break;
+                case 'p':
+                    echo '<p>'.'Programação de Computadores'.'</p>';
+                    break;
+                case 'w':
+                    echo '<p>'.'Programação Web'.'</p>';
+                    break;
+                
+                default:
+                    echo '<p>'.'Digite um código válido (m, r, p ou w)'.'</p>';
+                    break;
+            }
 
-        //do {
-            //$aluno = $grupo[$i];
-            //echo $aluno.'<br>';
-            //$i++;
-        //} while ($i < count($grupo));
+            //vetor
+            //matriz
+            //$grupo = array("Álvaro", "Kelly", "Letícia", "Rodrigo");
+            //$i = 0;
 
-        $grupo = array("Álvaro", "Kelly", "Letícia", "Rodrigo");
+            //do {
+                //$aluno = $grupo[$i];
+                //echo $aluno.'<br>';
+                //$i++;
+            //} while ($i < count($grupo));
 
-        for ($i=0; $i < count($grupo); $i++) { 
-            $aluno = $grupo[$i];
-            echo $aluno.'<br>';
-        }
+            $grupo = array("Álvaro", "Kelly", "Letícia", "Rodrigo");
 
-        $disciplinas = array("Redes de Computadores", "Algorítmos", "Programação de Computadores", "Manutenção de Computadores");
-        foreach ($disciplinas as $materias) {
-            echo '<p>'.$materias.'</p>';
-        }
+            for ($i=0; $i < count($grupo); $i++) { 
+                $aluno = $grupo[$i];
+                echo $aluno.'<br>';
+            }
 
-        $x = 'sistema acadêmico';
-        // echo strlen($x);
-        echo ucfirst($x);
+            $disciplinas = array("Redes de Computadores", "Algorítmos", "Programação de Computadores", "Manutenção de Computadores");
+            foreach ($disciplinas as $materias) {
+                echo '<p>'.$materias.'</p>';
+            }
 
-        $texto = 'Sistema Acadêmico: divulgação de nota dos alunos';
-        echo $texto;
-        echo str_replace('nota', 'média', $texto);
+            $x = 'sistema acadêmico';
+            // echo strlen($x);
+            echo ucfirst($x);
+
+            $texto = 'Sistema Acadêmico: divulgação de nota dos alunos';
+            echo $texto;
+            echo str_replace('nota', 'média', $texto);
 
 
-        function imprimirMedia($prova1, $prova2){
-            $media = ($prova1 + $prova2)/2;
-            echo 'A média do aluno é: '.$media;
-        }
+            function imprimirMedia($prova1, $prova2){
+                $media = ($prova1 + $prova2)/2;
+                echo 'A média do aluno é: '.$media;
+            }
 
-        imprimirMedia(7.0, 8.0);
+            imprimirMedia(7.0, 8.0);
 
-        imprimirMedia(10, 7);
-    ?>
-</body>
+            imprimirMedia(10, 7);
+        ?>
+    </body>
 </html>
