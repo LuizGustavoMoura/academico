61240354bacb1af58bcea18ef8a9c14d1ade3448
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-05 19:09:00 -0300

    login e index

--- dashboard.php
+++ dashboard.php
@@ -8,41 +8,46 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema Acadêmico</title>
-    <link rel="stylesheet" href="css/principal.css">
-</head>
-<body id="inicio">
-    <img src="imagens/logosenacv2.png" alt="logo senac" width="200px" height="auto">
-    <h1>Controle Acadêmico</h1>
-    <p>Bem-vindo ao <b>sistema de controle acadêmico</b>.</p> <p>Aqui, você poderá consultar suas <a href="nota.php" alt="notas da turma" target="_self">notas</a>.</p> <p>E também sua <i><a href="frequencia.php" alt="frequencia dos alunos">frequência</a></i>.</p>
-    <h2>Menu</h2>
-    <ul>
-        <li><a href="aluno.php">Alunos</a></li>
-        <li><a href="professor.php">Professores</a></li>
-        <li><a href="disciplina.php">Disciplinas</a></li>
-        <li><a href="turma.php">Turmas</a></li>
-        <li><a href="nota.php">Notas</a></li>
-        <li><a href="frequencia.php">Frequência</a></li>
-    </ul>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sistema Acadêmico</title>
+        <link rel="stylesheet" href="css/principal.css">
+    </head>
 
-    <?php
-        // Meu primeiro código
+    <body id="inicio">
+        <img src="imagens/logosenacv2.png" alt="logo senac" width="200px" height="auto">
 
-        echo '<h2>'.'Sistema Acadêmico'.'</h2>';
+        <h1>Controle Acadêmico</h1>
 
-        $aluno = '<p>'.'Luiz Gustavo'.'</p>';
-        $curso = "Análise e Desenvolvimento de Sistemas";
+        <p>Bem-vindo ao <b>sistema de controle acadêmico</b>.</p> <p>Aqui, você poderá consultar suas <a href="nota.php" alt="notas da turma" target="_self">notas</a>.</p> <p>E também sua <i><a href="frequencia.php" alt="frequencia dos alunos">frequência</a></i>.</p>
 
-        /* Esse é um comentário */
+        <h2>Menu</h2>
+        
+        <ul>
+            <li><a href="aluno.php">Alunos</a></li>
+            <li><a href="professor.php">Professores</a></li>
+            <li><a href="disciplina.php">Disciplinas</a></li>
+            <li><a href="turma.php">Turmas</a></li>
+            <li><a href="nota.php">Notas</a></li>
+            <li><a href="frequencia.php">Frequência</a></li>
+        </ul>
 
-        echo "O aluno: ".$aluno."Frequenta o curso: ".$curso;
+        <?php
+            // Meu primeiro código
 
-        $notas = array("prova1" => 8.3, "prova2" => 5.9, "prova3" => 9.2);
+            echo '<h2>'.'Sistema Acadêmico'.'</h2>';
 
-        echo '<p>'.($notas["prova1"]+$notas["prova2"]+$notas["prova3"])/3,'</p>';
-    ?>
-</body>
+            $aluno = '<p>'.'Luiz Gustavo'.'</p>';
+            $curso = "Análise e Desenvolvimento de Sistemas";
+
+            /* Esse é um comentário */
+
+            echo "O aluno: ".$aluno."Frequenta o curso: ".$curso;
+
+            $notas = array("prova1" => 8.3, "prova2" => 5.9, "prova3" => 9.2);
+
+            echo '<p>'.($notas["prova1"]+$notas["prova2"]+$notas["prova3"])/3,'</p>';
+        ?>
+    </body>
 </html>
