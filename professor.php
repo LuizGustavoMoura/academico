8d6bcd6ce6f0631701d1ed712c14988f9c378c7a
Luiz Gustavo Moura Gondim <luizgustavomoura2004@gmail.com>
2024-04-05 19:10:47 -0300

    professor

--- professor.php
+++ professor.php
@@ -1,29 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controle Acadêmico</title>
-    <link rel="stylesheet" href="css/principal.css">
-</head>
-<body id="professor">
-    <a href="index.php">Início</a>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Controle Acadêmico</title>
+        <link rel="stylesheet" href="css/principal.css">
+    </head>
 
-    <h1>Professores</h1>
-    
-    <table>
-        <tr>
-            <th>Disciplina</th>
-            <th>Professor(a)</th>
-        </tr>
-        <tr>
-            <td>Matemática</td>
-            <td>Professor Pedro do Vale</td>
-        </tr>
-        <tr>
-            <td>Português</td>
-            <td>Professora Bianca Lacerda</td>
-        </tr>
-    </table>
-</body>
+    <body id="professor">
+        <a href="index.php">Início</a>
+
+        <h1>Professores</h1>
+        
+        <table>
+            <tr>
+                <th>Disciplina</th>
+                <th>Professor(a)</th>
+            </tr>
+            <tr>
+                <td>Matemática</td>
+                <td>Professor Pedro do Vale</td>
+            </tr>
+            <tr>
+                <td>Português</td>
+                <td>Professora Bianca Lacerda</td>
+            </tr>
+        </table>
+    </body>
 </html>
